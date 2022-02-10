88d6118cf8ff59fb238901a892d00c8262fb4386
Konstantin Voykov <koykov0@gmail.com>
2022-02-11 00:16:07 +0200

    HTTP listener: follow signature changes.

--- listener/http.go
+++ listener/http.go
@@ -1,41 +1,61 @@
 package listener
 
 import (
+	"context"
+	"errors"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
-type HTTP struct{}
+type HTTP struct {
+	listener
+	srv *http.Server
+}
 
-func (l HTTP) Listen(addr string, out chan []byte) error {
-	var (
-		u   *url.URL
-		err error
-	)
-	u, err = url.Parse(addr)
+func (l HTTP) Listen(ctx context.Context, out chan []byte) error {
+	host, path, err := l.parseAddr()
 	if err != nil {
 		return err
 	}
-	var a string
-	if len(u.Scheme) > 0 {
-		a += u.Scheme + "://"
-	}
-	a += u.Host
-	p := u.Path
-	if !strings.HasPrefix(p, "/") {
-		p = "/" + p
-	}
 
-	http.HandleFunc(p, func(w http.ResponseWriter, req *http.Request) {
+	l.srv = &http.Server{Addr: host}
+
+	http.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
 		p, err := io.ReadAll(req.Body)
 		if err != nil {
 			log.Printf("err '%s' caught on request '%s'\n", err.Error(), req.RequestURI)
+			w.WriteHeader(http.StatusNotAcceptable)
 		} else {
 			out <- p
 		}
+		w.WriteHeader(http.StatusAccepted)
 	})
-	return http.ListenAndServe(a, nil)
+
+	go func() {
+		if err := l.srv.ListenAndServe(); err != nil {
+			log.Printf("server '%s' failed with err '%s'\n", host, err.Error())
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		err = l.srv.Shutdown(context.Background())
+	}
+	return err
+}
+
+func (l HTTP) parseAddr() (host, path string, err error) {
+	if len(l.addr) == 0 {
+		err = errors.New("empty address")
+		return
+	}
+	sli := strings.IndexByte(l.addr, '/')
+	if sli == -1 {
+		err = errors.New("bad address format")
+		return
+	}
+	host, path = l.addr[:sli], l.addr[sli:]
+	return
 }

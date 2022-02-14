a391e1ae550556f0d73b6dcc2d0e2021339dccbd
Konstantin Voykov <koykov0@gmail.com>
2022-02-15 01:00:46 +0200

    Get back traced project.

---
+++ cmd/traced/config.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	DB string `json:"db"`
+	UI string `json:"ui"`
+
+	BufSize uint `json:"buf_size"`
+	Workers uint `json:"workers"`
+
+	Listeners []Listener `json:"listeners"`
+	Notifiers []Notifier `json:"notifiers"`
+}
+
+type Listener struct {
+	Handler string `json:"handler"`
+	Addr    string `json:"addr"`
+}
+
+type Notifier struct {
+	Handler string `json:"handler"`
+	Addr    string `json:"addr"`
+}
+
+func ParseConfig(filepath string) (*Config, error) {
+	b, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	var c Config
+	if err = json.Unmarshal(b, &c); err != nil {
+		return nil, err
+	}
+	if c.BufSize == 0 {
+		c.BufSize = 1
+	}
+	if c.Workers == 0 {
+		c.Workers = 1
+	}
+	return &c, nil
+}
---
+++ cmd/traced/db.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jackc/pgx"
+)
+
+var dbi *sql.DB
+
+func dbConnect(addr string) (err error) {
+	var di int
+	if di = strings.Index(addr, "://"); di == -1 {
+		err = fmt.Errorf("couldn't get driver name from DSN '%s'", addr)
+		return
+	}
+	drv := addr[:di]
+	if dbi, err = sql.Open(drv, addr); err != nil {
+		return
+	}
+	if err = dbi.Ping(); err != nil {
+		return
+	}
+	return
+}

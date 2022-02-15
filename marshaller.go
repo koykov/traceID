e38cc0ae4f8949c1e1439dc235c3f8c108ae2601
Konstantin Voykov <koykov0@gmail.com>
2022-02-16 00:02:08 +0200

    Change marshaller signature.

--- marshaller.go
+++ marshaller.go
@@ -1,15 +1,28 @@
 package traceID
 
 import (
+	"fmt"
 	"io"
-
-	"github.com/koykov/traceID/marshaller"
 )
 
 type Marshaller interface {
 	Marshal(io.ReadWriter, interface{}) ([]byte, error)
+	MarshalIndent(io.ReadWriter, interface{}) ([]byte, error)
 }
 
 var (
-	defaultMarshaller = &marshaller.JSON{}
+	defaultMarshaller = &mfmt{}
 )
+
+type mfmt struct{}
+
+func (m mfmt) Marshal(rw io.ReadWriter, x interface{}) ([]byte, error) {
+	if _, err := fmt.Fprint(rw, x); err != nil {
+		return nil, err
+	}
+	return io.ReadAll(rw)
+}
+
+func (m mfmt) MarshalIndent(rw io.ReadWriter, x interface{}) ([]byte, error) {
+	return m.Marshal(rw, x)
+}
--- marshaller/json.go
+++ marshaller/json.go
@@ -9,8 +9,19 @@ import (
 
 type JSON struct{}
 
-func (m JSON) Marshal(rw io.ReadWriter, x interface{}) (b []byte, err error) {
+func (m JSON) Marshal(rw io.ReadWriter, x interface{}) ([]byte, error) {
+	return m.marshal(rw, x, false)
+}
+
+func (m JSON) MarshalIndent(rw io.ReadWriter, x interface{}) ([]byte, error) {
+	return m.marshal(rw, x, true)
+}
+
+func (m JSON) marshal(rw io.ReadWriter, x interface{}, indent bool) (b []byte, err error) {
 	e := json.NewEncoder(rw)
+	if indent {
+		e.SetIndent("", "\t")
+	}
 	if err = e.Encode(x); err != nil {
 		return
 	}

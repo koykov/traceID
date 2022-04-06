ed3bf49c06d1c487c01380af6204368aa8efa6dc
Konstantin Voykov <koykov0@gmail.com>
2022-04-07 00:31:25 +0300

    Dummy bench issue: measure allocation on With() method.

--- dummy_test.go
+++ dummy_test.go
@@ -6,11 +6,23 @@ import (
 	"time"
 )
 
+type testDEQInterface interface {
+	DeepEqual(interface{}, interface{}) bool
+}
+
+type testDEQ struct {
+}
+
+func (d testDEQ) DeepEqual(_, _ interface{}) bool { return true }
+
 func BenchmarkDummy(b *testing.B) {
 	b.Run("alloc-free", func(b *testing.B) {
+		optDEQ := Option("deq")
 		c := DummyClock{}
 		b.ReportAllocs()
 		for i := 0; i < b.N; i++ {
+			var opt testDEQInterface
+			opt = &testDEQ{}
 			dc := DummyCtx{}
 			dc.SetID("foobar").
 				SetService("server").
@@ -20,7 +32,7 @@ func BenchmarkDummy(b *testing.B) {
 				SetClock(c).
 				Debug("trace0").Var("v0", "foo").With(OptionIndent, true)
 			dc.Info("trace1").
-				Var("v1", math.MaxInt32).
+				Var("v1", math.MaxInt32).With(optDEQ, opt).
 				Var("v2", 256.56)
 			dc.Warn("trace2").
 				Slug("t2").
